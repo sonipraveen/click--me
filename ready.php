e9077b4528f5ac21f255f7ff97fb2177270a52ca
sonipraveen <66237747+sonipraveen@users.noreply.github.com>
2020-11-10 15:26:03 +0530

    Update ready.php

--- ready.php
+++ ready.php
@@ -6,7 +6,7 @@ $url = 'http://' . $_SERVER['HTTP_HOST'];
 <html>
 <head>
 <!---This Script Edited and Designed Kishan Raj ----->
-
+<script data-cfasync='false' type='text/javascript' src='//p416522.clksite.com/adServe/banners?tid=416522_819913_1&tagid=9'></script>
 
 
 <meta http-equiv="Content-Type" content="text/html; utf-8">
